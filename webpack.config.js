37381c8f0abeb055bb22a8901acd6f5277a5b8c2
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2022-03-21 00:09:13 +0700

    21.03.22

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 module.exports = {
     entry: './WEB/site/view/start/start_page_form.js',
     output: {
-        libraryTarget: 'var',
+        libraryTarget: 'umd',
         library: 'FormStart_module',
         path: __dirname + '/WEB/site/',
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        static : {
+          directory : __dirname + '/WEB/'
+        },
+        port: 3000,
+        devMiddleware:{
+           publicPath: "https://localhost:3000/",
+        },
+        hot: "only"
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
   };
