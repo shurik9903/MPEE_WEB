a4a6d3c1f79eecf1dfc0c9c6fbefd69465cdbd2d
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2022-04-20 23:53:16 +0700

    20.04.22

---
+++ view/registration/module/Registration.js
@@ -0,0 +1,36 @@
+"use strict";
+import {my_fetch_module} from '../../module/my_fetch';
+const my_fetch = my_fetch_module.my_fetch_async;
+
+let Registration_module = (()=>{
+
+    class Registration_class {
+
+        async Registration_async(RegData) {
+
+            let response = await my_fetch('registration/', { method: 'POST', headers: { 'Content-Type': 'application/json;charset=utf-8' }, body: JSON.stringify(RegData)});
+            //let response = await fetch('/MPEE/api/registration', { method: 'POST', headers: { 'Content-Type': 'application/json;charset=utf-8' }, body: JSON.stringify(RegData)});
+            
+            if (response.ok) {
+                let data = await response.json();
+                let Msg = "";
+
+                if (data.Msg)
+                    Msg = data.Msg;
+
+                return Msg;
+            } else throw Error("response.serverError" + response.errors);
+        };
+    };
+
+    let Registration = RegData => {
+        return new Registration_class().Registration_async(RegData);
+    }
+
+    return{
+        Registration_async: Registration
+    };
+
+})();
+
+export {Registration_module};
